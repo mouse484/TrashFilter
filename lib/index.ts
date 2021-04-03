ce040158d30ec36effe8628a6e80a1b093db543c
mouse_484 <escmouse484@gmail.com>
2021-04-03 10:48:50 +0000

    split file

---
+++ lib/index.ts
@@ -0,0 +1,40 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import axios from 'axios';
+
+const bad = (response: VercelResponse, arg?: string) => {
+  response.status(400).send('Bad Request' + arg);
+};
+
+export const makeFilter = async (
+  request: VercelRequest,
+  response: VercelResponse,
+  parser: (data: string) => string[],
+  type?: string
+): Promise<void> => {
+  const { url } = request.query as { url: string };
+  const Url = new URL(url);
+  if (url) {
+    const res = await axios(Url.pathname, {
+      baseURL: Url.origin,
+      proxy: false,
+    }).catch((err) => {
+      bad(response, url);
+      console.error(err);
+    });
+    if (res) {
+      const dates = res.data.split('\n') as string[];
+      const date = dates.flatMap((date) => parser(date)).join('\n');
+      response
+        .status(200)
+        .send(
+          [
+            '! Homepage: https://github.com/mouse484/TrashSiteFilter',
+            `! Title: Trash Site Filter (use:${url}${type ? `- ${type}` : ''})`,
+            date,
+          ].join('\n')
+        );
+    }
+  } else {
+    bad(response);
+  }
+};
