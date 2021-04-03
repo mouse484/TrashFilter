6034cee4ca2744f2bb84e6feb0132745c4482bef
mouse_484 <38714187+mouse484@users.noreply.github.com>
2021-04-03 20:01:54 +0900

    twitter trend (#2)
    fix #1

--- api/index.ts
+++ api/index.ts
@@ -1,45 +1,11 @@
-import { NowRequest, NowResponse } from '@vercel/node';
-import axios from 'axios';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { makeFilter } from '../lib/';
 
-const bad = (response: NowResponse, arg?: string) => {
-  response.status(400).send('Bad Request' + arg);
-};
-
-export default async (
-  request: NowRequest,
-  response: NowResponse
-): Promise<void> => {
-  const { url } = request.query as { url: string };
-  const Url = new URL(url);
-  if (url) {
-    const res = await axios(Url.pathname, {
-      baseURL: Url.origin,
-      proxy: false,
-    }).catch((err) => {
-      bad(response, url);
-      console.error(err);
-    });
-    if (res) {
-      const dates = res.data.split('\n') as string[];
-      const date = dates
-        .flatMap((date) => {
-          return [
-            date.includes('/') ? `${date}$document` : date,
-            `google.*##.g:has(a[href*="${date}"])`,
-          ];
-        })
-        .join('\n');
-      response
-        .status(200)
-        .send(
-          [
-            '! Homepage: https://github.com/mouse484/TrashSiteFilter',
-            `! Title: Trash Site Filter (use:${url})`,
-            date,
-          ].join('\n')
-        );
-    }
-  } else {
-    bad(response);
-  }
+export default (request: VercelRequest, response: VercelResponse): void => {
+  makeFilter(request, response, (data) => {
+    return [
+      data.includes('/') ? `${data}$document` : data,
+      `google.*##.g:has(a[href*="${data}"])`,
+    ];
+  });
 };
